92edc5c5b43df491402ba91c6b345d60528364cf
Rerisson Matos <rerisson.matos@splab.ufcg.edu.br>
2020-03-04 16:26:36 -0300

    With client implementation using vbase

--- node/clients/persistence.ts
+++ node/clients/persistence.ts
@@ -1,13 +1,25 @@
 import { InstanceOptions, IOContext, JanusClient, VBase } from '@vtex/api'
+import { Product as GraphQLProduct } from 'vtex.graphql-example'
 
-export default class Persistence extends JanusClient {
+export default class Product extends JanusClient {
+  private bucket = 'productsbucket'
+  private path = ''
+
   constructor(context: IOContext, options?: InstanceOptions) {
     super(context, {
       ...options,
     })
   }
 
-  public get(vbase: VBase) {
-    return vbase.getJSON<Product>()
+  public get(vbase: VBase, id: string) {
+    return vbase.getJSON<GraphQLProduct>(this.bucket, `${this.path}/${id}`)
+  }
+
+  public save(vbase: VBase, product: GraphQLProduct) {
+    return vbase.saveJSON<GraphQLProduct>(
+      this.bucket,
+      `${this.path}/${product.id}`,
+      product
+    )
   }
 }

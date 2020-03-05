20959504654a74a367c9fcd8d41f9291c34d5f0d
rerisson <rerissondaniel@gmail.com>
2020-03-04 22:59:18 -0300

    Add more stufffff

--- node/clients/persistence.ts
+++ node/clients/persistence.ts
@@ -1,9 +1,9 @@
-import { InstanceOptions, IOContext, JanusClient, VBase } from '@vtex/api'
-import { Product as GraphQLProduct } from 'vtex.graphql-example'
+import { InstanceOptions, IOContext, JanusClient, VBase, ID } from '@vtex/api'
+import { ProductInput, Product as GraphQLProduct } from 'vtex.graphql-example'
 
 export default class Product extends JanusClient {
   private bucket = 'productsbucket'
-  private path = ''
+  private path = 'rerisson'
 
   constructor(context: IOContext, options?: InstanceOptions) {
     super(context, {
@@ -11,15 +11,27 @@ export default class Product extends JanusClient {
     })
   }
 
-  public get(vbase: VBase, id: string) {
-    return vbase.getJSON<GraphQLProduct>(this.bucket, `${this.path}${id}`)
+  public async get(vbase: VBase, id: ID) {
+    const products = await this.getAll(vbase)
+    return products[+id]
   }
 
-  public save(vbase: VBase, product: GraphQLProduct) {
-    return vbase.saveJSON<GraphQLProduct>(
-      this.bucket,
-      `${this.path}${product.id}`,
-      product
-    )
+  public async getAll(vbase: VBase) {
+    try {
+      return await vbase.getJSON<GraphQLProduct[]>(this.bucket, this.path)
+    } catch (e) {
+      return []
+    }
+  }
+
+  public async save(vbase: VBase, product: ProductInput) {
+    const products = await this.getAll(vbase)
+    const newProduct: GraphQLProduct = { ...product, id: `${products.length}` }
+    await vbase.saveJSON<GraphQLProduct[]>(this.bucket, this.path, [
+      ...products,
+      newProduct,
+    ])
+
+    return { product, id: products.length }
   }
 }

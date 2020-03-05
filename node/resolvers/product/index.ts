8b572112c126ca81bac1e2c8a2b92d758808e652
rerisson <rerissondaniel@gmail.com>
2020-03-04 22:48:49 -0300

    Finish and improve

--- node/resolvers/product/index.ts
+++ node/resolvers/product/index.ts
@@ -1,12 +1,18 @@
 import { Product } from 'vtex.graphql-example'
+import { ID } from '@vtex/api'
 
 export const Query = {
-  product: (_: unknown, { id }: { id: string }, ctx: Context) => {
-    const {
-      clients: { persistence, vbase },
-    } = ctx
-    return persistence.get(vbase, id)
-  },
+  product: (
+    _: unknown,
+    { id }: { id: ID },
+    { clients: { persistence, vbase } }: Context
+  ) => persistence.get(vbase, id),
+
+  products: (
+    _: unknown,
+    __: undefined,
+    { clients: { persistence, vbase } }: Context
+  ) => persistence.getAll(vbase),
 }
 
 export const Mutation = {
